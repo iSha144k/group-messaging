39f7673bb5bea339a3aed2dd2886670bdab6c048
Iesha Osias <44386027+Queen-iSha@users.noreply.github.com>
2024-03-11 16:04:42 -0400

    adding tests to replies

--- cypress/e2e/replies.feature
+++ cypress/e2e/replies.feature
@@ -1,5 +1,69 @@
 Feature: Replies
 
-  Scenario: Access messages
+  Scenario: Read messages
     Given I am on the messages screen
-    Then I should see the messages screen
+    And I click the create button
+    And a message with "Message with Replies" as the title
+    When the message is sent
+    Then "Message with Replies" should be the title
+
+  Scenario: Disable replies
+    Given I am on the messages screen
+    And I open "Message with Replies"
+    When I open the options
+    And click Disable replies
+    And I click okay
+    Then the "Replies are turned off" message should appear
+
+  Scenario: Enable replies
+    Given I am on the messages screen
+    And I open "Message with Replies"
+    When I open the options
+    And click Enable replies
+    And I click okay
+    Then the "No replies" message should appear
+
+  Scenario: Reply to a message
+    Given I am on the messages screen
+    And I open "Message with Replies"
+    And I enter "Good" as a reply
+    When I send the reply
+    Then "Good" should appear as a reply
+
+  Scenario: Edit a reply
+    Given I am on the messages screen
+    And I open "Message with Replies"
+    When I open the reply options
+    And click Edit
+    And change the reply to "Great"
+    And click Save changes
+    Then "Great" should appear as a reply
+
+  Scenario: React to a reply
+    Given I am on the messages screen
+    And I open "Message with Replies"
+    When I click the reaction button
+    And select "❤️" as a reaction
+    Then "❤️" should appear as a reaction
+
+  Scenario: Change a reaction
+    Given I am on the messages screen
+    And I open "Message with Replies"
+    When I click the reaction button
+    And select "🙏️" as a reaction
+    Then "🙏" should appear as a reaction
+
+  Scenario: View Reactions
+    Given I am on the messages screen
+    And I open "Message with Replies"
+    When I open the reply options
+    And click View Reactions
+    Then the reactions should appear
+
+  Scenario: Delete a reply
+    Given I am on the messages screen
+    And I open "Message with Replies"
+    When I open the reply options
+    And click Delete
+    And I click okay
+    Then "Great" should not appear as a reply
--- cypress/e2e/replies.ts
+++ cypress/e2e/replies.ts
@@ -1,11 +1,106 @@
-import {Given, Then} from "@badeball/cypress-cucumber-preprocessor";
+import {Given, Then, When} from "@badeball/cypress-cucumber-preprocessor";
 
 
 Given("I am on the messages screen", () => {
     cy.visit("https://christ-ministry-420749.churchcenter.com/my/groups/test-group/messages");
 });
 
+Given("I click the create button", () => {
+    cy.contains('a', "New").click()
+});
 
-Then("I should see the messages screen", () => {
-    cy.contains('h1', "Messaging").should('exist')
+Given("a message with {string} as the title", (input: string) => {
+    cy.get('input[placeholder*="Title"]').type(input)
+});
+
+Given("I open {string}", (messageTitle: string) => {
+    cy.contains('h2', messageTitle).click()
+});
+
+Given("I enter {string} as a reply", (input: string) => {
+    cy.get('textarea[placeholder*="Message this group"]').type(input)
+});
+
+When("the message is sent", () => {
+    cy.contains('input', "Send").click()
+});
+
+When("I open the options", () => {
+    cy.contains('button', "Options").click()
+});
+
+When("click Disable replies", () => {
+    cy.contains('div', "Disable replies").click()
+});
+
+When("click Enable replies", () => {
+    cy.contains('div', "Enable replies").click()
+});
+
+When("I click okay", () => {
+    cy.on("window:confirm", (t) => {
+        cy.log(t)
+        return true;
+    });
+});
+
+When("I send the reply", () => {
+    cy.get('button[aria-label*="Submit message"]').click()
+});
+
+When("I open the reply options", () => {
+    cy.get('button[id*="menu-button--menu--81"]').click()
+});
+
+When("click Edit", () => {
+    cy.contains('div', "Edit").click()
+});
+
+When("change the reply to {string}", (input: string) => {
+    cy.contains('textarea', "Good").clear()
+    cy.get('.messaging__edit-reply-input').type(input)
+});
+
+When("click Save changes", () => {
+    cy.contains('button', "Save changes").click()
+});
+
+When("I click the reaction button", () => {
+    cy.get('button[aria-label*="Add reaction"]').click()
+});
+
+When("select {string} as a reaction", (reaction: string) => {
+    cy.contains("div", reaction).click()
+});
+
+When("click View Reactions", () => {
+    cy.contains('div', "View Reactions").click()
+});
+
+When("click Delete", () => {
+    cy.contains('div', "Delete").click()
+});
+
+Then("{string} should be the title", (expected: string) => {
+    cy.contains('h1', expected).should('exist')
+});
+
+Then("the {string} message should appear", (expected: string) => {
+    cy.contains('h3', expected).should('exist')
+});
+
+Then("{string} should appear as a reply", (expected: string) => {
+    cy.contains('p', expected).should('exist')
+});
+
+Then("{string} should appear as a reaction", (expected: string) => {
+    cy.contains('p', expected).should('exist')
+});
+
+Then("the reactions should appear", () => {
+    cy.contains('h3', "Reactions").should('exist')
+});
+
+Then("{string} should not appear as a reply", (expected: string) => {
+    cy.contains('p', expected).should('not.exist')
 });
